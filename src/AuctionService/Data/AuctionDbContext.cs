31ab6d732a4b7d106697882b2a2330b2f31fc931
VERTAFORE\aboukan <aaboukhalil@vertafore.com>
2023-10-27 10:40:55 -0400

    End of section 2!

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data;
+
+public class AuctionDbContext : DbContext
+{
+  public AuctionDbContext(DbContextOptions options) : base(options)
+  {
+  }
+  // we don't need to reference items in here becaise Auctions and items are related and entity framework will create a table for both 
+  public DbSet<Auction> Auctions { get; set; }
+}
